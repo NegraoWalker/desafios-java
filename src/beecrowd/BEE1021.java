7997ebaf5bc1adbaa7e34b769dda40fab71ffba1
walkerNegrão <walker.esteves.negrao@gmail.com>
2024-08-04 16:08:19 -0300

    AtDesafio BEE1001 resolvido

---
+++ src/beecrowd/BEE1021.java
@@ -0,0 +1,23 @@
+package beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class BEE1021 {
+    public static void main(String[] args) {
+        Locale.setDefault(new Locale("en","US"));
+        Scanner scanner = new Scanner(System.in);
+        /*
+            LER DOUBLE VALOR(DUAS CASAS DECIMAIS)
+            NOTAS 100, 50, 20, 10, 5, 2
+            MOEDAS 1, 0.50, 0.25, 0.10, 0.05 e 0.01
+
+
+         */
+
+
+
+
+        scanner.close();
+    }
+}
